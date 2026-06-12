4052e8aff18166e07d612ebc230ea8a5ad2c9935
Broey <jmontaro16@gmail.com>
2026-06-11 22:52:29 -0400

    Fix Vercel preview deployment

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 module.exports = (phase) => ({
   distDir: phase === PHASE_DEVELOPMENT_SERVER ? ".next-dev" : ".next",
   reactStrictMode: true,
+  experimental: {
+    outputFileTracingIgnores: ["**/public/**/*"],
+    outputFileTracingExcludes: {
+      "/": ["./public/**/*", "public/**/*", "**/public/**/*"],
+      "/about": ["./public/**/*", "public/**/*", "**/public/**/*"],
+      "/merch": ["./public/**/*", "public/**/*", "**/public/**/*"],
+      "/music": ["./public/**/*", "public/**/*", "**/public/**/*"],
+      "/music/[slug]": ["./public/**/*", "public/**/*", "**/public/**/*"],
+    },
+  },
   images: {
     remotePatterns: [
       {
